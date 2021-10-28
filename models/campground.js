ec592d1e0c6c673d4cff01eca0b5808fb0a67861
fyusufayan <f.yusuf88@gmail.com>
2021-10-28 14:04:19 +0300

    add review model

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongooes=require('mongoose');
+const Review=require('./review');
 const Schema=mongooes.Schema;
 
 const CampgroundSchema=new Schema({
@@ -6,8 +7,17 @@ const CampgroundSchema=new Schema({
     image:String,
     price:Number,
     description:String,
-    location:String
+    location:String,
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:'Review'
+    }]
 });
 
+CampgroundSchema.post('findOneAndDelete',async function(doc){
+    if(doc){
+        await Review.remove({_id:{$in:doc.reviews}})
+    }
+})
 
 module.exports=mongooes.model('Campground',CampgroundSchema);
