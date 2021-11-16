1175d6e0efc0dd90855828239374a03be56bb691
fyusufayan <f.yusuf88@gmail.com>
2021-11-16 16:27:54 +0300

    ready to deploy

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ const userRoutes=require('./routes/users')
 const campgroundRoutes=require('./routes/campgrounds');
 const reviewRoutes=require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+// const MongoDBStore=require("connect-mongo")(session);
+const MongoDBStore=require("connect-mongo");
+
+const dbUrl=process.env.DB_URL
+// const dbUrl='mongodb://localhost:27017/yelp-camp'
+
+mongoose.connect(dbUrl,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
@@ -89,9 +95,24 @@ app.use(
     })
 )
 
+const secret=process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store=MongoDBStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret
+    },
+    touchAfter: 24*60*60
+})
+
+store.on("error",function(e){
+    console.log("SESSION STORE ERROR!",e)
+})
+
 const sessionConfig={
+    store,
     name:'notdefaultsidname',
-    secret:'thisshouldbeabettersecret',
+    secret,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -101,8 +122,6 @@ const sessionConfig={
         maxAge:1000 *60*60*24*7
     }
 }
-
-
 
 app.use(session(sessionConfig));
 app.use(flash());
