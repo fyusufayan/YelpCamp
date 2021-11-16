bd18468ec5f25d460bf2a47986eb8605f66287c8
fyusufayan <f.yusuf88@gmail.com>
2021-11-16 12:52:23 +0300

    common security issues

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const passport=require('passport');
 const LocalStrategy=require('passport-local');
 const User=require('./models/user');
 
+const helmet=require('helmet');
+const mongoSanitize=require('express-mongo-sanitize');
+
 const userRoutes=require('./routes/users')
 const campgroundRoutes=require('./routes/campgrounds');
 const reviewRoutes=require('./routes/reviews');
@@ -37,14 +40,63 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
+app.use(mongoSanitize());
+app.use(helmet());//{contentSecurityPolicy:false}));
+
+const scriptSrcUrls=[
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/"
+]
+const styleSrcUrls=[
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/"
+]
+const connectSrcUrls=[
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/"
+]
+const fontSrcUrl=[];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives:{
+            defaultSrc:[],
+            connectSrc:["'self'",...connectSrcUrls],
+            scriptSrc:["'unsafe-inline'","'self'",...scriptSrcUrls],
+            styleSrc:["'unsafe-inline'","'self'",...styleSrcUrls],
+            workerSrc:["'self'","blob:"],
+            objectSrc:[],
+            imgSrc:[
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dou8gckhw/",
+                "https://images.unsplash.com",
+                "https://www.rei.com"
+            ],
+        }
+    })
+)
 
 const sessionConfig={
+    name:'notdefaultsidname',
     secret:'thisshouldbeabettersecret',
     resave:false,
     saveUninitialized:true,
     cookie:{
         httpOnly:true,
+        // secure:true,
         expires:Date.now()+1000 *60*60*24*7,
         maxAge:1000 *60*60*24*7
     }
